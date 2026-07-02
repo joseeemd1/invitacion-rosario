c31ae86b0ba14a369d6b054f8ded44193d4a3038
Joseeemd <joseeemd1@gmail.com>
2026-07-01 20:28:53 -0400

    Multi-domain rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        // Si el usuario entra a migraduacion.pro o www.migraduacion.pro
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: '(?<host>.*migraduacion\\.pro)',
+          },
+        ],
+        // Muestra silenciosamente la carpeta graduacion sin cambiar la URL
+        destination: '/graduacion',
+      },
+    ];
+  },
 };
 
 export default nextConfig;

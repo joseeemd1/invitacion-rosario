54c83bca16366bbc5c6e5f5e1693e99b301dd8d8
Joseeemd <joseeemd1@gmail.com>
2026-07-02 22:40:57 -0400

    fix: inyeccion de Mencion Especial #11 y resolucion de discrepancia en horario de itinerario

--- app/graduacion/data.ts
+++ app/graduacion/data.ts
@@ -18,6 +18,7 @@ export const MENCIONES_ESPECIALES = [
   { rol: "Director USAER", nombre: "Mtro. Eduardo Quiñonez Higuera" },
   { rol: "Maestro 6-A", nombre: "Juan Ángel Trejo Bujanda" },
   { rol: "Maestra 6-B", nombre: "Martha Celia Solis Hermosillo" },
+  { rol: "Pta. Sociedad de Padres", nombre: "Mayra Lizbeth Romero Vázquez" },
   { rol: "Pta. Sociedad de Alumnos", nombre: "Karol Guadalupe Sandoval Rochim" }
 ];
 
